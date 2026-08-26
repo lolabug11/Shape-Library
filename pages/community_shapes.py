54d291c54a600310cc5db569661d975e1817f5fd
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-08-26 19:09:40 +0000

    made it so you can see all the shapes in the database

--- pages/community_shapes.py
+++ pages/community_shapes.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import json
+from supabase_client import supabase
 st.header("Community Shape")
 if "last_page" not in st.session_state:
     st.session_state["last_page"] = 'community'
@@ -8,4 +10,53 @@ else:
 
 if st.button("Home"):
     st.switch_page('pages/home.py')
-st.write("Work in progress")
+
+
+
+class shape_card:
+
+    def __init__(self,name,formula,formula_with_correct_names,formula_type,number_of_inputs):
+        self.name = name
+        self.formula = formula
+        self.formula_with_correct_names = formula_with_correct_names
+        self.formula_type = formula_type
+        self.number_of_inputs = number_of_inputs
+
+    def create_card(self):
+        with st.container(border=True):
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader(self.name)
+                st.write(f"Type: {self.formula_type}")
+                st.write(f"Formula: {self.formula_with_correct_names} ")
+                st.write(f"Number of Inputs: {self.number_of_inputs}")
+            with col2:
+                st.subheader('')
+                if st.button("Use this shape"):
+                    st.write('WIP')
+
+    @staticmethod
+    def get_correct_names(formula,name_of_inputs: list):
+        x_identifyer = False
+        number_identifyer = False
+        new_formula = ''
+        for char in formula:
+            if char.lower() == "x":
+                x_identifyer = True
+            else:
+                if x_identifyer:
+                    print(name_of_inputs[0])
+                    new_formula += f' {name_of_inputs.pop(0)}'
+                    x_identifyer = False
+                else:
+                    new_formula += f' {char}'
+        return new_formula
+
+
+response = supabase.table("Shapes").select("*").execute()
+number_of_community_shapes = len(response.data)
+for shape in range(len(response.data)):
+    shape = response.data[shape]
+    formula_with_correct_names = shape_card.get_correct_names(shape['formula'],list(shape['name_of_inputs']))
+    shape_shapecard = shape_card(shape['name'],shape['formula'],formula_with_correct_names,shape['formula_type'],shape['number_of_inputs'])
+    shape_shapecard.create_card()
--- pages/make_a_shape.py
+++ pages/make_a_shape.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import json
 from supabase_client import supabase
 def test_formula(formula):
     return True
@@ -33,27 +33,37 @@ if st.session_state["reset_form"]:
     st.session_state["reset_form"] = False
 
 
-
+number_of_inputs = st.number_input(f"How many inputs does your formula have?",help= "Like for Base * height this would be 2. One for base, and one for height.",value=1,min_value= 1,key= 'number_of_inputs')
 with st.form("shape_creation_form"):
-    name = st.text_input("What is the name of your custom shape",key= 'name')
+    shape_name = st.text_input("What is the name of your custom shape",key= 'name')
     formula_type = st.selectbox(
         "Choose what you formula will say about your custom shape",
-        ["Area","Surface Area","Volume"],
+        ["Area","Perimiter","Surface Area","Volume"],
         key= 'formula_type'
     )
-    formula = st.text_input(f"What is the formula for your shape", help="Use 3.141 for pi. Seperate all signs by a space so like x * y / 6.",key= 'formula')
-    number_of_inputs = st.number_input(f"How many inputs does your formula have?",help= "Like for Base * height this would be 2. One for base, and one for height.",value=1,min_value= 1,key= 'number_of_inputs')
+    formula = st.text_input(f"What is the formula for your shape", help="Use 3.141 for pi. Seperate all signs by a space so like x * y / 6. Use x1 x2 x3 . . . instead of var names, youll name it later.",key= 'formula')
+    list_of_names = []
+    for i in range(number_of_inputs):
+        new_name = st.text_input(f"What is the x{i+1} input called?")
+        list_of_names.append(new_name)
 
     submitted = st.form_submit_button('Submit')
 if submitted:
     safe_formula = test_formula(formula)
+
     if safe_formula:
+        dict_of_names = {}
+        for name in range(len(list_of_names)):
+            name += 1
+            dict_of_names[list_of_names[name-1]] = f"x{name}"
+        json_of_names = dict_of_names
         supabase.table("Shapes").insert({
-            "name": name,
+            "name": shape_name,
             "formula": formula,
             "formula_type": formula_type,
-            "number_of_inputs": number_of_inputs
-    }).execute()
+            "number_of_inputs": number_of_inputs,
+            "name_of_inputs": json_of_names
+        }).execute()
         
         st.write("Your shape has been eccepted and submitted to the database!")
         st.session_state["reset_form"] = True
