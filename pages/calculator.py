e730406eb8f81f226efcaa957f3f3607279a4521
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-08-26 01:41:07 +0000

    fixed overflow error

--- pages/calculator.py
+++ pages/calculator.py
@@ -24,16 +24,26 @@ y = None
 z = None
 if default_shapes[shape]["number of inputs"] == 1:
     x = st.number_input(f'{default_shapes[shape]["name for the inputs"][0]}',value= 0.0,step= 0.001, format="%.3f",help= "Enter the input to you equation")
-    result = round(eval(default_shapes[shape]["formula"],{"x":x}),3)
-    st.write(f"The {default_shapes[shape]["formula type"]} of your {shape} is {result} units")
+    if x >= 2**64-1:
+        st.write("Your number is to large enter a new number")
+    else:
+        result = round(eval(default_shapes[shape]["formula"],{"x":x}),3)
+        st.write(f"The {default_shapes[shape]["formula type"]} of your {shape} is {result} units")
 elif default_shapes[shape]["number of inputs"] == 2:
     x = st.number_input(f'{default_shapes[shape]["name for the inputs"][0]}',value= 0.0,step= 0.001, format="%.3f",help= "Enter the first input to you equation")
     y = st.number_input(f'{default_shapes[shape]["name for the inputs"][1]}',value= 0.0,step= 0.001, format="%.3f",help= "Enter the second input to you equation",)
-    result = round(eval(default_shapes[shape]["formula"],{"x":x,"y":y}),3)
-    st.write(f"The {default_shapes[shape]["formula type"]} of your {shape} is {result} units")
+    if x >= 2**64-1 or y>= 2**64-1:
+        st.write("One of your inputs is to large enter smaller inputs")
+    else:
+        result = round(eval(default_shapes[shape]["formula"],{"x":x,"y":y}),3)
+        st.write(f"The {default_shapes[shape]["formula type"]} of your {shape} is {result} units")
 elif default_shapes[shape]["number of inputs"] == 3:
     x = st.number_input(f'{default_shapes[shape]["name for the inputs"][0]}',value= 0.0,step= 0.001, format="%.3f",help= "Enter the first input to you equation")
     y = st.number_input(f'{default_shapes[shape]["name for the inputs"][1]}',value= 0.0,step= 0.001, format="%.3f",help= "Enter the second input to you equation")
     z = st.number_input(f'{default_shapes[shape]["name for the inputs"][2]}',value= 0.0,step= 0.001, format="%.3f",help= "Enter the second input to you equation")
-    result = round(eval(default_shapes[shape]["formula"],{"x":x,"y":y,"z":z}),3)
-    st.write(f"The {default_shapes[shape]["formula type"]} of your {shape} is {result} units")
+    if x >= 2**64-1 or y>= 2**64-1 or z>=2**64-1:
+        st.write("One of your inputs is to large enter smaller inputs")
+    else:
+        result = round(eval(default_shapes[shape]["formula"],{"x":x,"y":y,z:"z"}),3)
+        st.write(f"The {default_shapes[shape]["formula type"]} of your {shape} is {result} units")
+    
