c64bf4212af23fd26e4b8bac37451616a5a97cc1
Lola Bug <139627294+lolabug11@users.noreply.github.com>
2026-08-30 17:17:02 +0000

    trying smth 2

--- pages/community_shapes.py
+++ pages/community_shapes.py
@@ -10,19 +10,20 @@ else:
 if "search" not in st.session_state:
     st.session_state["search"] = False
 
-if st.button("Home", key= "Home button Community"):
+if st.button("Home", key="Home button Community"):
     st.switch_page('pages/home.py')
 
-search = st.text_input("What shape do you want to search for?",value=" ")
+search = st.text_input("What shape do you want to search for?", value=" ")
 
 if "custom_shape_name" not in st.session_state:
     st.session_state["custom_shape_name"] = []
 if "custom_shape_properties" not in st.session_state:
     st.session_state["custom_shape_properties"] = []
 
+
 class shape_card:
 
-    def __init__(self,name,formula,formula_with_correct_names,formula_type,number_of_inputs,name_of_inputs,button_key):
+    def __init__(self, name, formula, formula_with_correct_names, formula_type, number_of_inputs, name_of_inputs, button_key):
         self.name = name
         self.formula = formula
         self.formula_with_correct_names = formula_with_correct_names
@@ -41,9 +42,10 @@ class shape_card:
                 st.write(f"Number of Inputs: {self.number_of_inputs}")
             with col2:
                 st.subheader('')
-                if st.button("Use this shape",key=self.button_key):
-                    st.session_state["custom_shape_name"].append( self.name + " (Custom)")
-                    st.session_state["custom_shape_properties"].append( {
+                if st.button("Use this shape", key=self.button_key):
+                    st.session_state["custom_shape_name"].append(
+                        self.name + " (Custom)")
+                    st.session_state["custom_shape_properties"].append({
                         "formula": self.formula,
                         "number of inputs": self.number_of_inputs,
                         "name for the inputs": self.name_of_inputs,
@@ -52,11 +54,8 @@ class shape_card:
                     st.session_state["using_custom"] = True
                     st.switch_page('pages/calculator.py',)
 
-
-
-
     @staticmethod
-    def get_correct_names(formula,name_of_inputs: list):
+    def get_correct_names(formula, name_of_inputs: list):
         x_identifyer = False
         number_identifyer = False
         new_formula = ''
@@ -74,7 +73,6 @@ class shape_card:
         return new_formula
 
 
-
 response = supabase.table("Shapes").select("*").execute()
 number_of_community_shapes = len(response.data)
 for shape in range(len(response.data)):
@@ -83,7 +81,8 @@ for shape in range(len(response.data)):
     shape['name'] += " "
     if search in shape['name'] or shape['name'] in search:
 
-        formula_with_correct_names = shape_card.get_correct_names(shape['formula'],list(shape['name_of_inputs']))
-
-        shape_shapecard = shape_card(shape['name'],shape['formula'],formula_with_correct_names,shape['formula_type'],shape['number_of_inputs'],list(shape['name_of_inputs']),f"{shape_id}{shape['name']}" )
+        formula_with_correct_names = shape_card.get_correct_names(
+            shape['formula'], list(shape['name_of_inputs']))
+        shape_shapecard = shape_card(shape['name'], shape['formula'], formula_with_correct_names, shape['formula_type'],
+                                     shape['number_of_inputs'], list(shape['name_of_inputs']), f"{shape_id}{shape['name']}")
         shape_shapecard.create_card()
